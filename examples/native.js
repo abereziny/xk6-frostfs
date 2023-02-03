nspcc-dev/xk6-neofs

[#50] Use `native.connect` timeouts everywhere
Also, use defaults from the SDK itself, allow user to provide 0.

Signed-off-by: Evgenii Stratonikov <e.stratonikov@yadro.com>

examples/native.js
@@ -3,7 +3,7 @@ import {fail} from "k6";
 import native from 'k6/x/neofs/native';
 
 const payload = open('../go.sum', 'b');
-const neofs_cli = native.connect("s01.neofs.devenv:8080", "1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb")
+const neofs_cli = native.connect("s01.neofs.devenv:8080", "1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb", 0, 0)
 
 export const options = {
     stages: [

examples/native_onsite.js
@@ -3,7 +3,7 @@ import native from 'k6/x/neofs/native';
 
 const payload = open('../go.sum', 'b');
 const container = "AjSxSNNXbJUDPqqKYm1VbFVDGCakbpUNH8aGjPmGAH3B"
-const neofs_cli = native.connect("s01.neofs.devenv:8080", "")
+const neofs_cli = native.connect("s01.neofs.devenv:8080", "", 0, 0)
 const neofs_obj = neofs_cli.onsite(container, payload)
 
 export const options = {

abereziny/xk6-frostfs

examples/native.js
@@ -3,7 +3,7 @@ import {fail} from "k6";
 import native from 'k6/x/frostfs/native';
 
 const payload = open('../go.sum', 'b');
-const frostfs_cli = native.connect("s01.frostfs.devenv:8080", "1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb")
+const frostfs_cli = native.connect("s01.frostfs.devenv:8080", "1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb", 0, 0)
 
 export const options = {
     stages: [

examples/native_onsite.js
@@ -3,7 +3,7 @@ import native from 'k6/x/frostfs/native';
 
 const payload = open('../go.sum', 'b');
 const container = "AjSxSNNXbJUDPqqKYm1VbFVDGCakbpUNH8aGjPmGAH3B"
-const frostfs_cli = native.connect("s01.frostfs.devenv:8080", "")
+const frostfs_cli = native.connect("s01.frostfs.devenv:8080", "", 0, 0)
 const frostfs_obj = frostfs_cli.onsite(container, payload)
 
 export const options = {